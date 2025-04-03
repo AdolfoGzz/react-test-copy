e54bee0295ee87d83d000bdd96db11436fbc3240
Adolfo Gonzalez <118020837+AdolfoGzz@users.noreply.github.com>
2025-04-03 16:09:01 -0600

    push 2

--- src/components/CustomSlider.jsx
+++ src/components/CustomSlider.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Slider } from "@mui/material";
 
 export default function CustomSlider({ value, onChange }) {
--- src/components/VariableBox.jsx
+++ src/components/VariableBox.jsx
@@ -1,4 +1,3 @@
-import React from "react"
 import CustomSlider from "./CustomSlider";
 import { Paper, Typography, Button } from "@mui/material";
 import { useState } from "react";
