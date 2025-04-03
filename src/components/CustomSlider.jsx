361cd9481302b55f9769de029f0a0bf80e8eb71a
Adolfo Gonzalez <118020837+AdolfoGzz@users.noreply.github.com>
2025-04-03 16:12:56 -0600

    push 3

--- src/components/CustomSlider.jsx
+++ src/components/CustomSlider.jsx
@@ -1,5 +1,20 @@
+import PropTypes from "prop-types";
 import { Slider } from "@mui/material";
 
 export default function CustomSlider({ value, onChange }) {
-  return <Slider value={value}onChange={onChange} min={0} max={100} step={1} sx={{ width: 200 }} />;
+  return (
+    <Slider
+      value={value}
+      onChange={onChange}
+      min={0}
+      max={100}
+      step={1}
+      sx={{ width: 200 }}
+    />
+  );
 }
+
+CustomSlider.propTypes = {
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
--- src/components/VariableBox.jsx
+++ src/components/VariableBox.jsx
@@ -6,7 +6,7 @@ export default function VariableBox() {
     const [value, setValue] = useState(50);
     return (
         <Paper sx={{ display: 'flex' , justifyContent: 'center', alignItems: 'center' , minWidth: 500, gap: 6, flexDirection: 'column', p:2, m:2}}>
-          <Typography variant="h5">Lorem Ipsum's: {value}</Typography>
+          <Typography variant="h5">Lorem Ipsum&apos;s: {value}</Typography>
           <Button variant="contained" color="primary" onClick={() => setValue(value + 10)}>
               More
           </Button>
